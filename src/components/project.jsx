4cc2b2d71e5a61ec3a81b735268bcbb2cf5b5f66
Dan Gherasimciuc <dan.gherasimciuc@savilleassessment.com>
2024-03-27 21:34:55 +0000

    Project

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import {useParams} from "react-router-dom";
+import React from "react";
+import { useParams } from "react-router-dom";
 import JsonData from "../data/data.json";
 const Project = () => {
-    let params = useParams()
+	let params = useParams();
 
-    return (
-        <div>
-
-            {JsonData.Gallery.map((d, i) => (
-                // d.title === params.projecId &&
-                    <div key={d.title}>
-                        {d.title}
-                        {d.description}
-                    </div>
-
-            ))}
-
-        </div>
-    );
+	return (
+		<div>
+			{JsonData.Gallery.filter((d) => d.title === params.projectId).map(
+				(d, i) => (
+					<div key={d.title}>
+						<h1>{d.title}</h1>
+						<p>{d.description}</p>
+						<img
+							src={d.largeImage}
+							alt=""
+						/>
+					</div>
+				)
+			)}
+		</div>
+	);
 };
 
 export default Project;
