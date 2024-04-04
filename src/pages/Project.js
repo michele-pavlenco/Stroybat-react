499c6594c5e17f9ff8303f097af8bafa4fb52338
Dan Gherasimciuc <dan.gherasimciuc@savilleassessment.com>
2024-04-05 00:39:59 +0100

    Swiper

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,61 +3,67 @@ import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import JsonData from "../data/data.json";
-import "../data/data.json";
-import "swiper/css";
-import "swiper/css/pagination";
+// import "swiper/css";
+// import "swiper/css/pagination";
 
 const Project = () => {
-	let params = useParams();
+	let { projectId } = useParams();
 	return (
 		<div>
-			{JsonData.Gallery.filter((d) => d.title === params.projectId).map((d) => (
-				<div
-					className="project"
-					key={d.title}>
-					<header
-						id="project-header"
-						style={{ width: "100%" }}>
-						<div
-							className="intro"
-							style={{
-								backgroundImage: `url(${d.productImage})`,
-								backgroundPosition: "center center",
-								backgroundRepeat: "no-repeat",
-							}}>
-							<div className="overlay">
-								<div className="col-md-8 col-md-offset-2 intro-text">
-									<h1>{d.title}</h1>
+			{JsonData.Gallery.filter((project) => project.title === projectId).map(
+				(project) => (
+					<div
+						className="project"
+						key={project.title}>
+						<header
+							id="project-header"
+							style={{ width: "100%" }}>
+							<div
+								className="intro"
+								style={{
+									backgroundImage: `url(${project.productImage})`,
+									backgroundPosition: "center center",
+									backgroundRepeat: "no-repeat",
+								}}>
+								<div className="overlay">
+									<div className="col-md-8 col-md-offset-2 intro-text">
+										<h1>{project.title}</h1>
+									</div>
 								</div>
 							</div>
-						</div>
-					</header>
+						</header>
 
-					<div className="project-description">
-						<div className="container">
-							<p>{d.description}</p>
+						<div className="project-description">
+							<div className="container">
+								<p>{project.description}</p>
+							</div>
 						</div>
-					</div>
-					<Swiper
-						pagination={true}
-						modules={[Pagination]}
-						className="mySwiper"
-						style={{
-							"--swiper-pagination-color": "#B50D04",
-						}}>
-						{d.images &&
-							d.images.map((image, index) => (
-								<SwiperSlide key={`image-${index}`}>
+						<Swiper
+							pagination={true}
+							modules={[Pagination]}
+							className="mySwiper"
+							style={{
+								"--swiper-pagination-color": "#B50D04",
+								width: "100vw",
+								height: "70vh",
+							}}>
+							{project.images.map((image, index) => (
+								<SwiperSlide key={`${project.title}-${index}`}>
 									<img
-										key={index}
 										src={image.url}
-										alt={`${d.title} - ${index + 1}`}
+										style={{
+											minHeight: "70vh",
+											width: "100%",
+											objectFit: "contain",
+										}}
+										alt={`${project.title} - ${index + 1}`}
 									/>
 								</SwiperSlide>
 							))}
-					</Swiper>
-				</div>
-			))}
+						</Swiper>
+					</div>
+				)
+			)}
 		</div>
 	);
 };
