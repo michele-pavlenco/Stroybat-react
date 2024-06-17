2922a77a2f3dd80e2a5a651241c9b9b64051ba07
michele-pavlenco <michelepavlenco.mp@gmail.com>
2024-06-17 22:26:05 +0200

    some fixes

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,21 +1,45 @@
 import React from "react";
+import { GiModernCity } from "react-icons/gi";
+import { GiBrickWall } from "react-icons/gi";
+import { BsBuildings } from "react-icons/bs";
 
 const Services = (props) => {
-	return (
-		<div
-			id="services"
-			className="text-center">
-			<div className="container">
-				<div className="section-title">
-					<h2>NOS SERVICES</h2>
-					<p>
-						Nous proposons une large gamme de services spécialisés en façade,
-						notamment le conseil, la conception, l'installation, la maintenance
-						et les réparations. Nous nous engageons à garantir des solutions sur
-						mesure et de haute qualité pour chaque projet.
-					</p>
-				</div>
-				<div className="row">
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>NOS SERVICES</h2>
+          <p>
+            Nous proposons une large gamme de services spécialisés en façade,
+            notamment le conseil, la conception, l'installation, la maintenance
+            et les réparations. Nous nous engageons à garantir des solutions sur
+            mesure et de haute qualité pour chaque projet.
+          </p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            {" "}
+           <GiModernCity/>
+            <div className="service-desc">
+              <h3>Facades Modernes</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            {" "}
+            <GiBrickWall/>
+            <div className="service-desc">
+              <h3>Bardage</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            {" "}
+			<BsBuildings/>
+            <div className="service-desc">
+              <h3>Vitrage</h3>
+            </div>
+          </div>
+        </div>
+        {/* <div className="row">
 					{props.data
 						? props.data.map((d, i) => (
 								<div
@@ -25,15 +49,15 @@ const Services = (props) => {
 									<i className={d.icon}></i>
 									<div className="service-desc">
 										<h3>{d.name}</h3>
-										<p>{d.text}</p>
+
 									</div>
 								</div>
 						  ))
 						: "loading"}
-				</div>
-			</div>
-		</div>
-	);
+				</div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
